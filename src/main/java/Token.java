7f24b686a3eec482d525d94b4c7567b2d7dacf54
ikierepka <ilona.kierepka@gmail.com>
2022-03-07 09:05:47 +0100

    add try catch for non numbers

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,20 +13,27 @@ public class Token {
         System.out.println("You can enter only 5, 10 or 15");
         tokenGenerator();
     }
+
     public static String tokenGenerator() {
         Scanner scanner = new Scanner(System.in);
-        int numberOfSigns = scanner.nextInt();
-        if (numberOfSigns == 5 || numberOfSigns == 10 || numberOfSigns == 15) {
-            for (int i = 0; i < numberOfSigns; ++i) {
-                char sign = allSigns.charAt(random.nextInt(SIZE));
-                TOKEN = TOKEN += sign;
+        try {
+            int numberOfSigns = scanner.nextInt();
+            if (numberOfSigns == 5 || numberOfSigns == 10 || numberOfSigns == 15) {
+                for (int i = 0; i < numberOfSigns; ++i) {
+                    char sign = allSigns.charAt(random.nextInt(SIZE));
+                    TOKEN = TOKEN += sign;
+                }
+                System.out.println("Your token is: " + TOKEN);
+                return TOKEN;
+            } else {
+                System.out.println("Enter a proper value: 5, 10 or 15");
+                System.out.println("Please try again");
             }
-            System.out.println("Your token is: " + TOKEN);
-            return TOKEN;
-        } else {
-            System.out.println("Enter a proper value: 5, 10 or 15");
-            System.out.println("Please try again");
+            return tokenGenerator();
+        } catch (InputMismatchException e) {
+            System.out.println("You can use integers only, please try again");
+            tokenGenerator();
         }
-        return tokenGenerator();
+        return TOKEN;
     }
 }
